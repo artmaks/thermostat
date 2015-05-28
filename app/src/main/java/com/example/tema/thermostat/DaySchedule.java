b774151bb71fec10e6d97dba29bf141816f9771a
artmaks <temamksimov@gmail.com>
2015-05-28 23:12:28 +0300

    All Activities

--- app/src/main/java/com/example/tema/thermostat/DaySchedule.java
+++ app/src/main/java/com/example/tema/thermostat/DaySchedule.java
@@ -37,10 +37,6 @@ public class DaySchedule extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         onBackPressed();
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/tema/thermostat/VacationMode.java
+++ app/src/main/java/com/example/tema/thermostat/VacationMode.java
@@ -29,10 +29,6 @@ public class VacationMode extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         onBackPressed();
         return super.onOptionsItemSelected(item);
